3f25920433ea7cc75c1f76615eaf3531213c1710
showeimer <showeimer@gmail.com>
2017-12-14 20:44:01 -0500

    finished activity, created three classes and called each method

--- src/Models.java
+++ src/Models.java
@@ -1,2 +1,47 @@
-public class Models {
+class Car {
+    String model;
+    int year;
+    boolean isHybrid;
+
+    public Car(String model, int year, boolean isHybrid) {
+        this.model = model;
+        this.year = year;
+        this.isHybrid = isHybrid;
+    }
+
+    public void printInfo() {
+        System.out.println("The car is a " + year + " " + model);
+    }
+}
+
+class Dog {
+    String name;
+    int age;
+    String breed;
+
+    public Dog(String name, int age, String breed) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+    }
+
+    public void printInfo() {
+        System.out.println(name + " is a " + age + " year old " + breed);
+    }
+}
+
+class Subject {
+    String subjectName;
+    String teacherName;
+    int courseLength;
+
+    public Subject(String subjectName, String teacherName, int courseLength) {
+        this.subjectName = subjectName;
+        this.teacherName = teacherName;
+        this.courseLength = courseLength;
+    }
+
+    public void printInfo() {
+        System.out.println("I am taking a " + courseLength + " week " + subjectName + " class with Professor " + teacherName);
+    }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,11 @@
 public class Runner {
 
     public static void main(String[] args) {
-        
+        Car myCar = new Car("Scion xD", 2010, false);
+        Dog myDog = new Dog("Misha", 6, "Shiba Inu");
+        Subject myClass = new Subject("Chemistry", "Xiu", 16);
+        myCar.printInfo();
+        myDog.printInfo();
+        myClass.printInfo();
     }
 }
